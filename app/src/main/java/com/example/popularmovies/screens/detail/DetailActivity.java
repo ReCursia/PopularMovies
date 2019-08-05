aac6f19adce78b2bbc0bdc81438ac87c18133df2
ReCursia <saha2098@yandex.ru>
2019-08-05 21:24:35 +0300

    commit

--- app/src/main/java/com/example/popularmovies/screens/detail/DetailActivity.java
+++ app/src/main/java/com/example/popularmovies/screens/detail/DetailActivity.java
@@ -3,16 +3,15 @@ package com.example.popularmovies.screens.detail;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
-import com.bumptech.glide.Glide;
 import com.example.popularmovies.R;
 import com.example.popularmovies.adapters.trailers.TrailersAdapter;
 import com.example.popularmovies.pojo.Movie;
@@ -24,9 +23,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class DetailActivity extends MvpAppCompatActivity implements DetailContract {
+    /*
     @BindView(R.id.titleTextView)
     TextView titleTextView;
     @BindView(R.id.descriptionTextView)
@@ -39,10 +38,18 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
     TextView releaseDateTextView;
     @BindView(R.id.posterImage)
     ImageView posterImage;
+    */
     @BindView(R.id.recyclerViewTrailers)
     RecyclerView recyclerView;
+    /*
     @BindView(R.id.favoriteIcon)
     ImageView favoriteIcon;
+    */
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsingToolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     @InjectPresenter
     DetailPresenter presenter;
 
@@ -52,11 +59,12 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
     public void setTrailers(List<Trailer> trailers) {
         adapter.setTrailers(trailers);
     }
-
+    /*
     @OnClick(R.id.favoriteIcon)
     public void onFavoriteIconClicked() {
         presenter.onFavoriteIconClicked();
     }
+    */
 
     @Override
     public void showErrorMessage(String message) {
@@ -71,7 +79,7 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
 
     @Override
     public void setFavoriteIconOn() {
-        favoriteIcon.setImageDrawable(getDrawable(R.drawable.favorite_icon_on));
+        //favoriteIcon.setImageDrawable(getDrawable(R.drawable.favorite_icon_on));
     }
 
     @Override
@@ -86,7 +94,7 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
 
     @Override
     public void setFavoriteIconOff() {
-        favoriteIcon.setImageDrawable(getDrawable(R.drawable.favorite_icon_off));
+        //favoriteIcon.setImageDrawable(getDrawable(R.drawable.favorite_icon_off));
     }
 
     @Override
@@ -96,11 +104,18 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
         ButterKnife.bind(this);
         initRecyclerView();
         initAdapter();
+        initCollapsingToolbarLayout();
+        setSupportActionBar(toolbar);
+    }
+
+    private void initCollapsingToolbarLayout() {
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.white));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.white));
     }
 
     @Override
     public void openTrailerUrl(int position) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(NetworkUtils.TRAILER_BASE_URL + adapter.getItem(position).getSite()));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(NetworkUtils.TRAILER_BASE_URL + adapter.getItem(position).getSite()));
         //TODO я думаю надо в презентер передавать TRAILER и уже там решать что делать... А не так как сейчас (аля логика в UI)
         startActivity(browserIntent);
     }
@@ -118,14 +133,16 @@ public class DetailActivity extends MvpAppCompatActivity implements DetailContra
     @Override
     public void setMovieDetail(Movie movie) {
         getSupportActionBar().setTitle(movie.getTitle());
-        titleTextView.setText(movie.getTitle());
-        originalTitleTextView.setText(movie.getOriginalTitle());
-        releaseDateTextView.setText(movie.getReleaseDate());
-        descriptionTextView.setText(movie.getOverview());
-        ratingTextView.setText(Double.toString(movie.getVoteAverage()));
+        //titleTextView.setText(movie.getTitle());
+        //originalTitleTextView.setText(movie.getOriginalTitle());
+        //releaseDateTextView.setText(movie.getReleaseDate());
+        //descriptionTextView.setText(movie.getOverview());
+        //ratingTextView.setText(Double.toString(movie.getVoteAverage()));
         //Image
+        /*
         Glide.with(this)
                 .load(NetworkUtils.BASE_POSTER_URL + NetworkUtils.BIG_POSTER_SIZE + movie.getPosterPath())
                 .into(posterImage);
+                */
     }
 }
