7f343bba8369783337a774ff613af9773a328d90
ReCursia <saha2098@yandex.ru>
2019-08-08 18:38:42 +0300

    Create MoviesFragment.java

---
+++ app/src/main/java/com/example/popularmovies/ui/fragments/MoviesFragment.java
@@ -0,0 +1,79 @@
+package com.example.popularmovies.ui.fragments;
+
+import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.example.popularmovies.pojo.Movie;
+import com.example.popularmovies.views.MainContract;
+
+import java.util.List;
+
+public class MoviesFragment extends MvpAppCompatFragment implements MainContract {
+    @Override
+    public void setSwitchOff() {
+        
+    }
+
+    @Override
+    public void setSwitchOn() {
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showErrorMessage(String message) {
+
+    }
+
+    @Override
+    public void setMovies(List<Movie> movies) {
+
+    }
+
+    @Override
+    public void addMovies(List<Movie> movies) {
+
+    }
+
+    @Override
+    public void openAboutDialog() {
+
+    }
+
+    @Override
+    public void hideAboutDialog() {
+
+    }
+
+    @Override
+    public void openGooglePlayPage() {
+
+    }
+
+    @Override
+    public void setPopularTextColor(int color) {
+
+    }
+
+    @Override
+    public void setRatedTextColor(int color) {
+
+    }
+
+    @Override
+    public void openDetailScreen(int position) {
+
+    }
+
+    @Override
+    public void openFavoriteScreen() {
+
+    }
+}
