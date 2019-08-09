2d848ff08a6a687d47301166da0f6a3fa8e38b47
ReCursia <saha2098@yandex.ru>
2019-08-09 18:38:29 +0300

    Update MoviesFragment.java

--- app/src/main/java/com/example/popularmovies/ui/fragments/MoviesFragment.java
+++ app/src/main/java/com/example/popularmovies/ui/fragments/MoviesFragment.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
 import com.arellomobile.mvp.presenter.InjectPresenter;
-import com.arellomobile.mvp.presenter.PresenterType;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 import com.example.popularmovies.R;
 import com.example.popularmovies.network.MoviesService;
