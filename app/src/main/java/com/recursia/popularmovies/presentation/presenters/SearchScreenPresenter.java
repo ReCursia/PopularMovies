1bd7e40fecba475f7a7518e48ba879484c9396d7
ReCursia <saha2098@yandex.ru>
2020-01-06 04:43:03 +0300

    live search with debounce implemented

--- app/src/main/java/com/recursia/popularmovies/presentation/presenters/SearchScreenPresenter.java
+++ app/src/main/java/com/recursia/popularmovies/presentation/presenters/SearchScreenPresenter.java
@@ -7,21 +7,49 @@ import com.recursia.popularmovies.domain.models.Movie;
 import com.recursia.popularmovies.presentation.views.SearchScreenContract;
 import com.recursia.popularmovies.utils.LangUtils;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.Subject;
 
 @InjectViewState
 public class SearchScreenPresenter extends MvpPresenter<SearchScreenContract> {
-    private static final int TIMEOUT = 500; //ms
-    private static final int QUERY_PAGE = 1;
+    private final static int TIMEOUT = 300;
+    private final static TimeUnit TIME_UNIT = TimeUnit.MILLISECONDS;
+    private final static int QUERY_PAGE = 1;
     private final SearchScreenInteractor searchScreenInteractor;
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private final Subject<String> subject = BehaviorSubject.create();
 
     public SearchScreenPresenter(SearchScreenInteractor searchScreenInteractor) {
         this.searchScreenInteractor = searchScreenInteractor;
+    }
+
+    @Override
+    protected void onFirstViewAttach() {
+        initLiveSearch();
+    }
+
+    private void initLiveSearch() {
+        Disposable d = subject
+                .debounce(TIMEOUT, TIME_UNIT, AndroidSchedulers.mainThread())
+                .distinctUntilChanged()
+                .subscribe(this::updateDisplayedList);
+        compositeDisposable.add(d);
+    }
+
+    private void updateDisplayedList(String query) {
+        Disposable d = searchScreenInteractor
+                .getMoviesByQuery(query, QUERY_PAGE, LangUtils.getDefaultLanguage())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        (m) -> getViewState().setMovies(m),
+                        (t) -> getViewState().showErrorMessage(t.getLocalizedMessage())
+                );
+        compositeDisposable.add(d);
     }
 
     @Override
@@ -30,25 +58,14 @@ public class SearchScreenPresenter extends MvpPresenter<SearchScreenContract> {
         compositeDisposable.dispose();
     }
 
-    private void handleErrorMessage(Throwable throwable) {
-        getViewState().showErrorMessage(throwable.getLocalizedMessage());
-    }
-
     public void onItemClicked(Movie item) {
         getViewState().openDetailScreen(item);
     }
 
-    public void onQueryTextChanged(String s) {
-        //TODO make debounce, it's a complicated..
-        Disposable d = searchScreenInteractor
-                .getMoviesByQuery(s, QUERY_PAGE, LangUtils.getDefaultLanguage())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::handleSearchResults, this::handleErrorMessage);
-        compositeDisposable.add(d);
-    }
-
-    private void handleSearchResults(List<Movie> movies) {
-        getViewState().setMovies(movies);
+    public void onQueryTextChanged(String query) {
+        if (!query.isEmpty()) {
+            subject.onNext(query);
+        }
     }
 
 }
